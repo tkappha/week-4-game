32ef9df4879d61be8a996a6fb20ac99d25755727
Tami <tkapphahn@gmail.com>
2017-05-14 19:34:04 -0700

    still working.. getting closer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,29 @@
 // ********** VARIABLES ***********************
 
 //The number the player will try to reach
-var numberToMatch="";
+var targetScore = 0;
+//player's score
+var currentScore = 0;
 
-var playerScore = 0;
 
-var pinkCrystal;
-var greenCrystal;
-var redCrystal;
-var purpleCrystal;
+var crystal = {
+	pink:
+	{	name: "Pink",
+		value: 0
+	},
+	green:
+	{	name: "Green",
+		value: 0
+	},
+	red: 
+	{	name: "Red",
+		value: 0
+	},
+	purple:
+	{	name: "Purple",
+		value: 0
+	}
+}
 
 var winCount = 0;
 var lossCount = 0;
@@ -18,48 +33,68 @@ var lossCount = 0;
 // *******    FUNCTIONS  ****************
 
 //generates values of crystals
-function crystalValue(crystal) {
-	return crystal = Math.ceil(Math.random()*12);
+var getRandom = function(min, max){
+	return Math.floor(Math.random()*(max-min+1)) + min; 
 }
 
-function startGame(){
-	//generates value of number player is trying to match
-	return numberToMatch = Math.floor(Math.random()*101) + 19;
-	
+//starts game
+var startGame = function() {
+	//resets
+	var currentScore = 0;
+	targetScore = getRandom(19, 120);
 
-	
+	//set crystal values
+	crystal.pink.value = getRandom(1,12);
+	crystal.green.value  = getRandom(1,12);
+	crystal.red.value = getRandom(1,12);
+	crystal.purple.value = getRandom(1,12);
+
+	$("#yourScore").html(currentScore);
+	$("#targetScore").html(targetScore);
+}
+
+//changes score when buttons clicked
+var addValues = function(crystal){
+	currentScore = currentScore + crystal.value;
+
+	$("#yourScore").html(currentScore);
+	//checks to see if game is over
+	checkWin();
+}
+
+//check for win/loss
+var checkWin = function() {
+	if(currentScore > targetScore){
+		alert("You lose!");
+		lossCount++;
+		$("#losses").html(lossCount);
+
+	} else if(currentScore == targetScore){
+		alert("You win!");
+		winCount++;
+		$("#wins").html(winCount);
+	}
 
 
-	//resets for game
-	playerScore = 0;
-	$("#playerScore").text(playerScore);
+}
+// ********** Main Process *******************
 
-	$("button").on("click", function(){
-		playerScore = playerScore + parseInt($(this).data("number"));
+startGame();
 
-		$("playerScore").text(playerScore);
+$("#pinkCrystal").click(function(){
+	addValues(crystal.pink);
+});
 
-		if(number === numberToMatch){
-			winCount++;
-			alert("you win!");
-			startGame();
-			crystalValue();
-		} else if (number > numberToMatch){
-			lossCount++;
-			alert("you lose!");
-			startGame();
-			crystalValue();
-		}
+$("#greenCrystal").click(function(){
+	addValues(crystal.green);
+});
 
+$("#redCrystal").click(function(){
+	addValues(crystal.red);
+});
 
-
-	});
-
-};
-
-$("#value").text(numberToMatch);
-
-
-
+$("#purpleCrystal").click(function(){
+	addValues(crystal.purpleCrystal);
+});
 
 
